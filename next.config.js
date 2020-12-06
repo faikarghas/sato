6ea4e13c3abf43e9282c3755820b62b51f2b154e
faikar <ghassanfaikar@yahoo.com>
2020-12-06 09:20:23 +0700

    test pwa

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
     pwa: {
         disable: process.env.NODE_ENV === 'development',
-        // dest: 'public', // comment out this line
+        dest: 'public', // comment out this line
         register: true,
         sw: '/sw.js',
         scope:'/'
